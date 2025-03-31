dfc97d5da41037e4eb129f8ee77b5074b1ceb52a
HabiburRahmanZihad <e241024@ugrad.iiuc.ac.bd>
2025-03-31 07:04:16 +0600

    fixing some issue

---
+++ Week 01/day-1/first code.c
@@ -0,0 +1,12 @@
+/*
+This is my first C program.
+It prints "Hello, World!" to the console.
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World\n");
+    printf("Hello, C programming\n");
+    return 0;
+}
